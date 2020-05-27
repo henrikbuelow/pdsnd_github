9a20b62e32f990daa570701d05e3f5b415fbd741
Henrik Buelow <henrikbuelow@gmx.de>
2020-05-27 18:29:39 +0200

    added a comment in the main-function where other functions are called

--- bikeshare.py
+++ bikeshare.py
@@ -333,6 +333,7 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
+        # call functions for statistical calculations
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
